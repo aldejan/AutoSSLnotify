f7d1475cb005904c57c3d4ec7fc97a92db6155d6
Aldejan Lourenco <66024189+aldejan@users.noreply.github.com>
2022-10-18 17:14:15 -0300

    Update notify.php

--- notify.php
+++ notify.php
@@ -2,20 +2,20 @@
   include "config.php";
 
   // 3 - resgatar o assunto digitado no formulário e  grava na variavel //$assunto
-  $assunto = "Seu SSL foi Renovado Automaticamente!";
+  $assunto = "Verifique seus Certificados SSLs!";
    //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
   $mensagem = "Ola Admin<br /><br />
-  Parabens! seus Certificados SSLs do site ".$site." foram renovados automaticamentes.<br />
-  Verifique esse linck <a target='_black' href='https://".$site."/AutoSSLnotify/sslChecker.php'>https://".$site."/AutoSSLnotify/sslChecker.php</a> para ver se a açao realmente aconteceu.<br /><br />
- For more information access ".$site."";
+  Verifique se seus Certificados SSLs foram estao Renovados.<br />
+  Acesse esse link <a target='_black' href='https://".$site."/AutoSSLnotify/sslChecker.php'>https://".$site."/AutoSSLnotify/sslChecker.php</a> para ver se a açao realmente aconteceu.<br /><br />
+ Este Email é enviado automaticamente todo Mes";
 
 //5 – agora inserimos as codificações corretas e  tudo mais.
   $headers =  "Content-Type:text/html; charset=UTF-8\n";
-  $headers .= "From: Auto SSL ".$site." <autossl@".$site.">\n"; //Vai ser //mostrado que  o email partiu deste email e seguido do nome
-  $headers .= "X-Sender:  <autossl@".$site.">\n"; //email do servidor //que enviou
+  $headers .= "From: Auto SSL ".$site." <sistemas@".$site.">\n"; //Vai ser //mostrado que  o email partiu deste email e seguido do nome
+  $headers .= "X-Sender:  <sistemas@".$site.">\n"; //email do servidor //que enviou
   $headers .= "X-Mailer: PHP  v".phpversion()."\n";
   $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-  $headers .= "Return-Path:  <autossl@".$site."\n"; //caso a msg //seja respondida vai para  este email.
+  $headers .= "Return-Path:  <sistemas@".$site."\n"; //caso a msg //seja respondida vai para  este email.
   $headers .= "MIME-Version: 1.0\n";
 
 mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
