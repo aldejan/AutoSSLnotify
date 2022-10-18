d2ca8f42092f8618e92e086b7a11d7d6b7ebf944
Aldejan Lourenco <66024189+aldejan@users.noreply.github.com>
2022-10-18 17:33:42 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 $para = "username@gmail.com"; // pode usar varios emails separados por virgula
+$remetente = "seuremetente@gmail.com"; //escreve o email remetente
 $site = "seudominio.com"; // sem www
 //edite tambem sslChecker.ini.php
 ?>
